70bc812b5f5e82995527074228bca3816c9175b4
Saurav Kumar <saurav_1901ee54@iitp.ac.in>
2021-10-30 19:39:30 +0530

    init

--- sendemail.py
+++ sendemail.py
@@ -8,28 +8,29 @@ smtp_server = "smtp.gmail.com"
 sender_email = "mail.checker.iitp@gmail.com"
 password = 'Saurav*9113'
 
-try:
-    with open('./input/responses.csv','r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
-        for data in reader:
-            if data[6]=='Roll Number' or data[6]=='ANSWER':
-                continue
-            mail_content = "Dear "+data[3]+""",\nCS384 2021 marks are attached for reference.\n\nDr. Mayank"""
-            message = EmailMessage()
-            message['From'] = sender_email
-            message['To'] = data[1].strip()
-            message['Cc'] = data[4].strip()
-            message['Subject'] = 'Python Mark Sheet'
-            message.set_content(mail_content)
-            marksheet_name = data[6]+'.xlsx'
-            marksheet = open('./output/'+marksheet_name, 'rb')
-            marks = marksheet.read()
-            message.add_attachment(marks, maintype='application',subtype='octet-stream', filename=marksheet_name)
-            print(data[6])
+
+with open('./input/responses.csv','r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+    for data in reader:
+        if data[6]=='Roll Number' or data[6]=='ANSWER':
+            continue
+        mail_content = "Dear "+data[3]+""",\nCS384 2021 marks are attached for reference.\n\nDr. Mayank"""
+        message = EmailMessage()
+        message['From'] = sender_email
+        message['To'] = data[1].strip()
+        message['Cc'] = data[4].strip()
+        message['Subject'] = 'Python Mark Sheet'
+        message.set_content(mail_content)
+        marksheet_name = data[6]+'.xlsx'
+        marksheet = open('./output/'+marksheet_name, 'rb')
+        marks = marksheet.read()
+        message.add_attachment(marks, maintype='application',subtype='octet-stream', filename=marksheet_name)
+        try:
             server = smtplib.SMTP_SSL(smtp_server, port)
             server.login(sender_email, password)
             server.send_message(message)
-    server.quit()
+            server.quit()
+        except Exception as e:
+            print(data[6])
+    
 
-except Exception as e:
-    print(e)
